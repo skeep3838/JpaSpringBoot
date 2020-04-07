42a809489179609f9264d50a5caed081dbf6fe71
Nicole Lin <sleep3838@gmail.com>
2020-04-07 09:11:05 +0800

    no message

--- src/main/java/com/controller/ItemlineComtroller.java
+++ src/main/java/com/controller/ItemlineComtroller.java
@@ -1,17 +1,14 @@
 package com.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.model.Customer;
-import com.model.Itemline;
 import com.model.Orders;
+import com.service.CustomerService;
 import com.service.ItemlineService;
 import com.service.OrderService;
 
